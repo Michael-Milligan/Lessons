ad8d6cfce98f86df1aae674e980ff02d65b0b5c7
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2020-03-19 09:35:26 +0300

    Added Unity Lessons

--- C-like lessons/CS lessons/Lessons/Program.cs
+++ C-like lessons/CS lessons/Lessons/Program.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Threading;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
 
 class Program
 { 
     static void Main()
     {
-        
+        using (StreamWriter Writer = new StreamWriter("C:\\Users\\User\\Desktop\\1.txt", true, Encoding.UTF8))
+        {
+            Writer.WriteLine("Hello, Admin");
+            Writer.Write("Hello, User");
+        }
+        using (StreamReader Reader = new StreamReader("C:\\Users\\User\\Desktop\\1.txt"))
+        {
+            MessageBox.Show(Reader.ReadToEnd(), "Info");
+        }
     }
 }
