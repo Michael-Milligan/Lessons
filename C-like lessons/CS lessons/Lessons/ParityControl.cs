793aaf3796ab03d8a390503eed6cc75a008db86d
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2020-05-04 19:18:13 +0300

    Begin of task 33

---
+++ C-like lessons/CS lessons/Lessons/ParityControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Lessons
+{
+    class ParityControl
+    {
+        public static void Main()
+        {
+            string Path = "Resources.txt";
+
+            int[] Numbers = File.ReadAllText(Path).
+                Split().
+                Select(number => Convert.ToInt32(Convert.ToString(Convert.ToInt32(number), 2))).
+                ToArray();
+
+            for (int i = 0; i < Numbers.Length; ++i)
+            {
+                if
+            }
+        }
+    }
+}
