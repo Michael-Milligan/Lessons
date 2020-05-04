947f9eb5144dca12bc3c0eda3a28b82a5d70d62f
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2020-05-04 23:54:56 +0300

    Task ParityControl finished

--- C-like lessons/CS lessons/Lessons/ParityControl.cs
+++ C-like lessons/CS lessons/Lessons/ParityControl.cs
@@ -11,14 +11,42 @@ namespace Lessons
         {
             string Path = "Resources.txt";
 
-            int[] Numbers = File.ReadAllText(Path).
+            string[] Numbers = File.ReadAllText(Path).
                 Split().
-                Select(number => Convert.ToInt32(Convert.ToString(Convert.ToInt32(number), 2))).
+                Select(number => (Convert.ToString(Convert.ToInt32(number), 2))).
                 ToArray();
 
             for (int i = 0; i < Numbers.Length; ++i)
             {
-                if
+                Numbers[i] = Numbers[i].PadLeft(8, '0');
+            }
+
+            string[] SubNumbers = new string[Numbers.Length];
+
+            for (int i = 0; i < SubNumbers.Length; ++i)
+            {
+                SubNumbers[i] = Numbers[i].Clone() as string;
+                SubNumbers[i] = SubNumbers[i].Remove(0, 1);
+            }
+
+            int Sum = 0; 
+            string Remainder = "";
+
+            for (int i = 0; i < Numbers.Length; ++i)
+            {
+                Sum = SubNumbers[i].Sum(digit => Convert.ToInt32(digit) - '0');
+                Remainder = Convert.ToString((Sum) % 2);
+                if (Remainder != Convert.ToString(Numbers[i][0]))
+                    SubNumbers[i] = "-1";
+            }
+
+            SubNumbers = SubNumbers.Where(item => item != "-1").ToArray();
+            int Code = 0;
+
+            foreach (var item in SubNumbers)
+            {
+                Code = Convert.ToInt32(item, 2);
+                Console.Write((char)Code);
             }
         }
     }
