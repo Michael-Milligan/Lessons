352b9846ceae20a9bfc885d2d28089645509b8a2
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2020-05-05 17:38:22 +0300

    re-install

---
+++ C-like lessons/CS lessons/Lessons/BinarySearch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Lessons
+{
+    class BinarySearch
+    {
+        public static void Main()
+        {
+            string Path = "Resources.txt";
+            int[] Input = File.ReadAllLines(Path)[0].
+                Split().
+                Select(number => Convert.ToInt32(number)).
+                ToArray();
+
+            int[] Numbers = new int[Input[0]];
+
+            
+        }
+    }
+}
---
+++ C-like lessons/CS lessons/Lessons/RotateString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace Lessons
+{
+    class RotateString
+    {
+        public static void Main()
+        {
+            string Path = "Resources.txt";
+            string[] RawInput = File.ReadAllLines(Path);
+            string[][] Input = new string[RawInput.Length - 1][];
+
+            for (int i = 0; i < Input.Length; ++i)
+            {
+                Input[i] = RawInput[i + 1].Split();
+                Methods.RotateString(ref Input[i][1], Convert.ToInt32(Input[i][0]));
+                Console.WriteLine(Input[i][1]);
+            }
+            
+        }
+    }
+}
