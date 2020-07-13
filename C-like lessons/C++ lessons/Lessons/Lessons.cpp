0c98eb250300fe28e50583c5321fdef56a287007
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2020-07-14 00:58:44 +0300

    Revert "Something gone wrong"
    This reverts commit 85cb544a86a3b77cb0263bc336818f2bf92b0fc1.

--- C-like lessons/C++ lessons/Lessons/Lessons.cpp
+++ C-like lessons/C++ lessons/Lessons/Lessons.cpp
@@ -8,15 +8,4 @@ int main()
 	return 0;
 }
 
-///<summary>
-///Returns the same number
-///</summary>
-///<param name="i:"> Just some number</param>
-///<param name="j:"> Just another number</param>
-///<returns>Exactly the same number without any operations</returns>
-int GetInt(int i)
-{
-	return i;
-}
-
 
