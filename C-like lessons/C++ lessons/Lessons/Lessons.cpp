386b41507666574116930780a5bdbb3702c0e0cb
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2020-07-16 19:17:51 +0300

    Cleaned Lessons

--- C-like lessons/C++ lessons/Lessons/Lessons.cpp
+++ C-like lessons/C++ lessons/Lessons/Lessons.cpp
@@ -2,10 +2,21 @@
 #include <list>
 using namespace std;
 
+int get_result(int number, bool sign);
+
 int main()
 {
-
+	cout << get_result(1, true);
 	return 0;
 }
 
-
+/// <summary>
+/// Returns exactly the same value of the parameter with the sign chosen
+/// </summary>
+/// <param name="number: ">The number which must be returned</param>
+/// <param name="sign: ">The sign of the result: true for -, false for +</param>
+/// <returns>The same number with the chosen sign</returns>
+int get_result(int number, bool sign)
+{
+	return number * (int)sign * (-1);
+}
