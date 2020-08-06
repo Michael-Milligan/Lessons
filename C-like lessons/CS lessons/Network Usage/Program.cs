a1ac4f185773edafb7f283296f0439bd82671797
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2020-08-06 19:29:33 +0300

    Began learning ASP.NET Core

--- C-like lessons/CS lessons/Network Usage/Program.cs
+++ C-like lessons/CS lessons/Network Usage/Program.cs
@@ -13,17 +13,7 @@ namespace Network_Usage
             HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create("https://www.profinance.ru/currency_eur.asp");
             HttpWebResponse webResponse = (HttpWebResponse)webRequest.GetResponse();
 
-            string Html;
-            using (var Content = webResponse.GetResponseStream())
-            {
-                using (var Reader = new StreamReader(Content))
-                {
-                    Html = Reader.ReadToEnd();
-                }
-            }
-            Regex Query = new Regex(@"<td class=cell align=center colspan=.2.><font color=.Red.><b>(\d{2}\.\d{4})");
-            var Matches = Query.Matches(Html);
-            Console.WriteLine(Matches[0].Groups[1]);
+            
         }
     }
 }
