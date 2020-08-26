f8853bf97b2b2bd16720cde8d6a96d3619895321
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2020-08-26 21:42:36 +0300

    Made a multi thread client-server app

--- C-like lessons/CS lessons/Client/Client.cs
+++ C-like lessons/CS lessons/Client/Client.cs
@@ -14,29 +14,44 @@ namespace Client
         static void Main()
         {
             IPEndPoint LocalEndPoint = new IPEndPoint(IPAddress.Parse(IP), Port);
-            TcpClient Client = new TcpClient();
-            Client.Connect(LocalEndPoint);
-            NetworkStream Stream = Client.GetStream();
 
-            byte[] Buffer = new byte[1024];
-            Console.WriteLine("Enter your message: ");
-
-            Buffer = Encoding.UTF8.GetBytes(Console.ReadLine());
-            StringBuilder Message = new StringBuilder();
-
-            Stream.Write(Buffer);
-            Buffer = new byte[1024];
-
-            do
+            TcpClient Client = null;
+            try
             {
-                int Length = Stream.Read(Buffer, 0, Buffer.Length);
-                Message.Append(Encoding.UTF8.GetString(Buffer, 0, Length));
-            }
-            while (Stream.DataAvailable);
-            Console.WriteLine(Message);
+                Console.Write("Type your name here: ");
+                string Name = Console.ReadLine();
+                while (true)
+                {
+                    Client = new TcpClient();
+                    Client.Connect(LocalEndPoint);
+                    NetworkStream Stream = Client.GetStream();
 
-            Stream.Dispose();
-            Stream.Close();
+                    byte[] Buffer = new byte[1024];
+                    Console.WriteLine("\nEnter your message: ");
+
+                    Buffer = Encoding.UTF8.GetBytes($"{Name}: {Console.ReadLine()}");
+                    StringBuilder Message = new StringBuilder();
+
+                    Stream.Write(Buffer);
+                    Buffer = new byte[1024];
+
+                    do
+                    {
+                        int Length = Stream.Read(Buffer, 0, Buffer.Length);
+                        Message.Append(Encoding.UTF8.GetString(Buffer, 0, Length));
+                    }
+                    while (Stream.DataAvailable);
+                    Console.WriteLine($"Server: {Message}");
+                }
+            }
+            catch(SocketException Exception)
+            {
+                Console.WriteLine($"The error: {Exception.Message}\nThe Stacktrace: {Exception.StackTrace}\nThe code of error: {Exception.SocketErrorCode}");
+            }
+            finally
+            {
+                if (Client != null) Client.Close();
+            }
             Console.ReadLine();
         }
     }
--- C-like lessons/CS lessons/Server/Server.cs
+++ C-like lessons/CS lessons/Server/Server.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace Server
 {
@@ -11,35 +12,77 @@ namespace Server
         static int Port = 8005;
         static string IP = "127.0.0.1";
 
+        class ClientObject
+        {
+            public TcpClient Client;
+
+            public ClientObject(TcpClient client)
+            {
+                Client = client ?? throw new ArgumentNullException(nameof(client));
+            }
+
+            public void Process()
+            {
+                NetworkStream Stream = null;
+                try
+                {
+                    while (true)
+                    {
+                        Stream = Client.GetStream();
+                        StringBuilder Message = new StringBuilder();
+                        byte[] Buffer = new byte[1024];
+
+                        do
+                        {
+                            int Length = Stream.Read(Buffer);
+                            Message.Append(Encoding.UTF8.GetString(Buffer, 0, Length));
+                        }
+                        while (Stream.DataAvailable);
+
+                        DateTime Now = DateTime.Now;
+                        Console.WriteLine($"{Now.Day}/{Now.Month}/{Now.Year} {Now.Hour}:{Now.Minute} {Now.Second}s| {Message}");
+                        Stream.Write(Encoding.UTF8.GetBytes("Your message was delivered to server"));
+                    }
+                }
+                catch (SocketException Exception)
+                {
+                    Console.WriteLine($"The error: {Exception.Message}\nThe Stacktrace: {Exception.StackTrace}\nThe code of error: {Exception.SocketErrorCode}");
+                }
+                finally
+                {
+                    if (Stream != null) Stream.Close();
+                    if (Client != null) Client.Close();
+                }
+            }
+        }
+
         static void Main()
         {
             IPEndPoint LocalEndPoint = new IPEndPoint(IPAddress.Parse(IP), Port);
 
-            TcpListener Server = new TcpListener(LocalEndPoint);
-
-            Console.WriteLine("Waiting for connections...");
-            Console.WriteLine("The connection dates and messages:");
-            Server.Start(10);
-
-            while (true)
+            try
             {
-                TcpClient Handler = Server.AcceptTcpClient();
-                NetworkStream Stream = Handler.GetStream();
-                StringBuilder Message = new StringBuilder();
-                byte[] Buffer = new byte[1024];
+                TcpListener Server = new TcpListener(LocalEndPoint);
 
-                do
+                Console.WriteLine("Waiting for connections...");
+                Console.WriteLine("The connection dates and messages:");
+                Server.Start(10);
+
+                while (true)
                 {
-                    int Length = Stream.Read(Buffer);
-                    Message.Append(Encoding.UTF8.GetString(Buffer, 0, Length));
+                    TcpClient Client = Server.AcceptTcpClient();
+                    ClientObject Object = new ClientObject(Client);
+                    Thread NewThread = new Thread(Object.Process);
+                    NewThread.Start();
                 }
-                while (Handler.Available > 0);
-
-                DateTime Now = DateTime.Now;
-                Console.WriteLine($"{Now.Day}/{Now.Month}/{Now.Year} {Now.Hour}:{Now.Minute} {Now.Second}s: {Message}");
-                Stream.Write(Encoding.UTF8.GetBytes("Your message was delivered"));
-                Handler.Dispose();
-                Handler.Close();
+            }
+            catch (SocketException Exception)
+            {
+                Console.WriteLine($"The error: {Exception.Message}\nThe Stacktrace: {Exception.StackTrace}\nThe code of error: {Exception.SocketErrorCode}");
+            }
+            finally
+            {
+                
             }
         }
     }
