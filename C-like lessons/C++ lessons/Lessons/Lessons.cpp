7100debd52186c2c4bd5732135372e58d8cb46e0
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2020-09-27 14:39:55 +0300

    Preparation for 3d task

--- C-like lessons/C++ lessons/Lessons/Lessons.cpp
+++ C-like lessons/C++ lessons/Lessons/Lessons.cpp
@@ -5,56 +5,6 @@
 #include "BinaryTree.cpp"
 using namespace std;
 
-double Factorial(int Number)
-{
-	double result = 1;
-	if (Number < 0)
-	{
-		cout << "Error! You must enter natural number only";
-		return -1;
-	}
-	for (double i = 1; i <= Number; i++)
-	{
-		result *= i;
-	}
-	return result;
-}
-
-//int factorial(int n)
-//{
-//	return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
-//}
-
-
-bool CheckforSimplicity(int Number)
-{
-	if (Number == 1) return true;
-	for (int i = 2; i < Number; i++)
-	{
-		if (Number % i == 0) return false;
-	}
-	return true;
-}
-
-void SwapDigits(int Number)
-{
-	int Remainder = 0;
-	if (Number == 0)
-		cout << "Error";
-	else
-	{
-		for (; Number % 10 == 0; )
-			Number = Number / 10;
-		while (Number != 0)
-		{
-			Remainder = Number % 10;
-			Number = Number / 10;
-			cout << Remainder;
-		}
-	}
-	return;
-}
-
 int main()
 {
 	/*srand(time(0));
@@ -98,18 +48,6 @@ int main()
 		cout << endl;
 	}*/
 
-	double x, a = 1, e = 0;
-	double b, c;
-	double eps;
-	cin >> x >> eps;
-	for (int i = 1; abs(a) >= eps; i++)
-	{
-		e += a;
-		b = pow(x, i);
-		c = Factorial(i);
-		a = b / c;
-	}
-	cout << e;
 
 
 	return 0;
--- C-like lessons/CS lessons/Yandex Cup/Program.cs
+++ C-like lessons/CS lessons/Yandex Cup/Program.cs
@@ -10,191 +10,157 @@ namespace Yandex_Cup
 {
     class Program
     {
-        static Query[] Queries;
-        static Document[] Documents;
-        static Host[] Hosts;
+        
         static void Main(string[] args)
         {
-            Queries = ReadQueries();
-            Documents = ReadDocuments();
-            Hosts = ReadHosts();
-
-            var Groups = Hosts.GroupBy(item => item.HostID);
-            List<Document> DistinctDocs = new List<Document>();
-            foreach (var group in Groups)
-            {
-                var temp = group.ToArray();
-                var Docs = GetDocsFromURLs(temp.Select(item => item.DocumentURL).ToArray()).ToArray();
-                DistinctDocs.Add(Docs.Where(item => item.Relevance == Docs.Max(item => item.Relevance)).ToArray()[0]);
-            }
-
-            var DocGroups = DistinctDocs.GroupBy(item => item.QueryID).ToArray();
-            var Max = new
-            {
-                Query = "",
-                Found = 0.0
-            };
-            foreach (var group in DocGroups)
-            {
-                var temp = pFound(group);
-                if (temp > Max.Found)
-                {
-                    Max = new
-                    {
-                        Query = group.Key,
-                        Found = temp
-                    };
-                }
-            }
-            Console.WriteLine(Queries.First(item => item.ID == Max.Query).Text);
-
+            
         }
 
         #region 1
+        //public class Order
+        //{
+        //    public string Spectacle { get; set; }
+        //    public string PhoneNumber { get; set; }
+        //}
 
-        
-        public class Order
-        {
-            public string Spectacle { get; set; }
-            public string PhoneNumber { get; set; }
-        }
+        //public static List<Order> ReadCsv(string Path)
+        //{
+        //    string[] RawData = File.ReadAllLines(Path);
+        //    string[][] Data = new string[RawData.Length][];
+        //    List<Order> Results = new List<Order>();
 
-        public static List<Order> ReadCsv(string Path)
-        {
-            string[] RawData = File.ReadAllLines(Path);
-            string[][] Data = new string[RawData.Length][];
-            List<Order> Results = new List<Order>();
+        //    for (int i = 0; i < Data.GetLength(0); i++)
+        //    {
+        //        Data[i] = RawData[i].Split(',');
+        //        Results.Add(new Order()
+        //        {
+        //            Spectacle = Data[i][0],
+        //            PhoneNumber = Data[i][1]
+        //        });
+        //    }
+        //    return Results;
+        //}
 
-            for (int i = 0; i < Data.GetLength(0); i++)
-            {
-                Data[i] = RawData[i].Split(',');
-                Results.Add(new Order()
-                {
-                    Spectacle = Data[i][0],
-                    PhoneNumber = Data[i][1]
-                });
-            }
-            return Results;
-        }
+        //public static void MakePhonesSameFormat(List<Order> Orders)
+        //{
+        //    Regex Query = new Regex(@".*(\d).*(\d{3}).*(\d{3}).*(\d{2}).*(\d{2})");
+        //    foreach (var order in Orders)
+        //    {
+        //        var match = Query.Match(order.PhoneNumber);
+        //        order.PhoneNumber = $"{match.Groups[1]}{match.Groups[2]}{match.Groups[3]}{match.Groups[4]}{match.Groups[5]}";
+        //    }
+        //}
 
-        public static void MakePhonesSameFormat(List<Order> Orders)
-        {
-            Regex Query = new Regex(@".*(\d).*(\d{3}).*(\d{3}).*(\d{2}).*(\d{2})");
-            foreach (var order in Orders)
-            {
-                var match = Query.Match(order.PhoneNumber);
-                order.PhoneNumber = $"{match.Groups[1]}{match.Groups[2]}{match.Groups[3]}{match.Groups[4]}{match.Groups[5]}";
-            }
-        }
-
-        public static int FindMaxUniqueNumbers(List<Order> Orders)
-        {
-            var Groups = Orders.GroupBy(item => item.Spectacle).ToArray();
-            int Max = 0;
-            for (int i = 0; i < Groups.Count(); ++i)
-            {
-                var temp = Groups[i].ToArray().Distinct().ToArray();
-                Max = temp.Length > Max ? temp.Length : Max;
-            }
-            return Max;
-        }
+        //public static int FindMaxUniqueNumbers(List<Order> Orders)
+        //{
+        //    var Groups = Orders.GroupBy(item => item.Spectacle).ToArray();
+        //    int Max = 0;
+        //    for (int i = 0; i < Groups.Count(); ++i)
+        //    {
+        //        var temp = Groups[i].ToArray().Distinct().ToArray();
+        //        Max = temp.Length > Max ? temp.Length : Max;
+        //    }
+        //    return Max;
+        //}
         #endregion
 
-        public class Query
-        {
-            public string ID { get; set; }
-            public string Text { get; set; }
-        }
+        #region 2
+        //public class Query
+        //{
+        //    public string ID { get; set; }
+        //    public string Text { get; set; }
+        //}
 
-        public class Document
-        {
-            public string QueryID { get; set; }
-            public string URL { get; set; }
-            public string Relevance { get; set; }
-        }
+        //public class Document
+        //{
+        //    public string QueryID { get; set; }
+        //    public string URL { get; set; }
+        //    public string Relevance { get; set; }
+        //}
 
-        public class Host : IEquatable<Host>
-        {
-            public string HostID { get; set; }
-            public string DocumentURL { get; set; }
+        //public class Host : IEquatable<Host>
+        //{
+        //    public string HostID { get; set; }
+        //    public string DocumentURL { get; set; }
 
-            public bool Equals([AllowNull] Host other)
-            {
-                return other.HostID == HostID;
-            }
-        }
+        //    public bool Equals([AllowNull] Host other)
+        //    {
+        //        return other.HostID == HostID;
+        //    }
+        //}
 
-        public static Query[] ReadQueries()
-        {
-            string[] RawData = File.ReadAllLines(@"D:\qid_query.tsv");
-            string[][] Data = new string[RawData.Length][];
-            Query[] Results = new Query[Data.GetLength(0)];
-            for (int i = 0; i < RawData.Length; ++i)
-            {
-                Results[i] = new Query();
-                Data[i] = RawData[i].Split('\t');
-                Results[i].ID = Data[i][0];
-                Results[i].Text = Data[i][1];
-            }
-            return Results;
-        }
+        //public static Query[] ReadQueries()
+        //{
+        //    string[] RawData = File.ReadAllLines(@"D:\qid_query.tsv");
+        //    string[][] Data = new string[RawData.Length][];
+        //    Query[] Results = new Query[Data.GetLength(0)];
+        //    for (int i = 0; i < RawData.Length; ++i)
+        //    {
+        //        Results[i] = new Query();
+        //        Data[i] = RawData[i].Split('\t');
+        //        Results[i].ID = Data[i][0];
+        //        Results[i].Text = Data[i][1];
+        //    }
+        //    return Results;
+        //}
 
-        public static Document[] ReadDocuments()
-        {
-            string[] RawData = File.ReadAllLines(@"D:\qid_url_rating.tsv");
-            string[][] Data = new string[RawData.Length][];
-            Document[] Results = new Document[Data.GetLength(0)];
-            for (int i = 0; i < RawData.Length; ++i)
-            {
-                Results[i] = new Document();
-                Data[i] = RawData[i].Split('\t');
-                Results[i].QueryID = Data[i][0];
-                Results[i].URL = Data[i][1];
-                Results[i].Relevance = Data[i][2];
-            }
-            return Results;
-        }
+        //public static Document[] ReadDocuments()
+        //{
+        //    string[] RawData = File.ReadAllLines(@"D:\qid_url_rating.tsv");
+        //    string[][] Data = new string[RawData.Length][];
+        //    Document[] Results = new Document[Data.GetLength(0)];
+        //    for (int i = 0; i < RawData.Length; ++i)
+        //    {
+        //        Results[i] = new Document();
+        //        Data[i] = RawData[i].Split('\t');
+        //        Results[i].QueryID = Data[i][0];
+        //        Results[i].URL = Data[i][1];
+        //        Results[i].Relevance = Data[i][2];
+        //    }
+        //    return Results;
+        //}
 
-        public static Host[] ReadHosts()
-        {
-            string[] RawData = File.ReadAllLines(@"D:\hostid_url.tsv");
-            string[][] Data = new string[RawData.Length][];
-            Host[] Results = new Host[Data.GetLength(0)];
-            for (int i = 0; i < RawData.Length; ++i)
-            {
-                Results[i] = new Host();
-                Data[i] = RawData[i].Split('\t');
-                Results[i].HostID = Data[i][0];
-                Results[i].DocumentURL = Data[i][1];
-            }
-            return Results;
-        }
+        //public static Host[] ReadHosts()
+        //{
+        //    string[] RawData = File.ReadAllLines(@"D:\hostid_url.tsv");
+        //    string[][] Data = new string[RawData.Length][];
+        //    Host[] Results = new Host[Data.GetLength(0)];
+        //    for (int i = 0; i < RawData.Length; ++i)
+        //    {
+        //        Results[i] = new Host();
+        //        Data[i] = RawData[i].Split('\t');
+        //        Results[i].HostID = Data[i][0];
+        //        Results[i].DocumentURL = Data[i][1];
+        //    }
+        //    return Results;
+        //}
 
-        public static IEnumerable<Document> GetDocsFromURLs(string[] URLs)
-        {
-            foreach (var url in URLs)
-            {
-                yield return Documents.Where(item => item.URL == url).ElementAt(0);
-            }
-        }
+        //public static IEnumerable<Document> GetDocsFromURLs(string[] URLs)
+        //{
+        //    foreach (var url in URLs)
+        //    {
+        //        yield return Documents.Where(item => item.URL == url).ElementAt(0);
+        //    }
+        //}
 
-        public static double pFound(IGrouping<string, Document> group)
-        {
-            var Query = group.Key;
-            var Docs = group.ToArray().OrderByDescending(item => item.Relevance).ToArray().Take(10).ToArray();
+        //public static double pFound(IGrouping<string, Document> group)
+        //{
+        //    var Query = group.Key;
+        //    var Docs = group.ToArray().OrderByDescending(item => item.Relevance).ToArray().Take(10).ToArray();
 
-            double Sum = 0;
-            for (int i = 0; i < Docs.Length; i++)
-            {
-                Sum += pLook(Docs, i) * Convert.ToDouble(Docs[i].Relevance.Replace('.',','));
-            }
-            return Sum;
-        }
+        //    double Sum = 0;
+        //    for (int i = 0; i < Docs.Length; i++)
+        //    {
+        //        Sum += pLook(Docs, i) * Convert.ToDouble(Docs[i].Relevance.Replace('.',','));
+        //    }
+        //    return Sum;
+        //}
 
-        public static double pLook(Document[] Docs, int Index)
-        {
-            if (Index == 0) return 1;
-            return pLook(Docs, Index - 1) * (1 - Convert.ToDouble(Docs[Index - 1].Relevance.Replace('.',','))) * (1 - 0.15);
-        }
+        //public static double pLook(Document[] Docs, int Index)
+        //{
+        //    if (Index == 0) return 1;
+        //    return pLook(Docs, Index - 1) * (1 - Convert.ToDouble(Docs[Index - 1].Relevance.Replace('.',','))) * (1 - 0.15);
+        //}
+        #endregion
     }
 }
