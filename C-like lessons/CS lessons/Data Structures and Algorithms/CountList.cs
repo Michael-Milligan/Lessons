97fd5ebb9f5e32c2aa6150a8a349c3791e9969b6
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2020-11-08 12:21:02 +0300

    Began implementing CountList

---
+++ C-like lessons/CS lessons/Data Structures and Algorithms/CountList.cs
@@ -0,0 +1,29 @@
+﻿namespace Data_Structures_and_Algorithms
+{
+    class CountList<T>
+    {
+        CountListNode<T> _Head;
+
+        public void Add(T Data)
+        {
+            if (_Head == null)
+            {
+                _Head = new CountListNode<T>(Data);
+                return;
+            }
+
+
+        }
+    }
+
+    class CountListNode<T>
+    {
+        T _Data;
+        CountListNode<T> pNext;
+
+        public CountListNode(T data)
+        {
+            _Data = data;
+        }
+    }
+}
--- C-like lessons/CS lessons/Data Structures and Algorithms/Program.cs
+++ C-like lessons/CS lessons/Data Structures and Algorithms/Program.cs
@@ -6,24 +6,7 @@ namespace Data_Structures_and_Algorithms
     {
         static void Main()
         {
-            SkipList<int> SL = new SkipList<int>(5);
-            for (int i = 1; i <= 25; ++i)
-            {
-                SL.Add(i);
-            }
-            SL.PrintList();
-            SL.Delete(4);
-            Console.WriteLine();
-            SL.PrintList();
-            Console.WriteLine();
-            SL.Add(80);
-            SL.PrintList();
-
-            Console.WriteLine();
-            SL.Add(70);
-            SL.PrintList();
-
-            //TODO: Make Add fill spaces made by Delete
+            
         }
     }
 }
