fdb7379dc1ccc2d701afd7c55df080cd9e393b55
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2021-01-01 12:43:44 +0300

    Began learning ASP.NET Core MVC

---
+++ Web/WebApplications/MVC/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm(GuestResponse Response)
+        {
+            //TODO: store response from the guest
+            return View();
+        }
+    }
+}
