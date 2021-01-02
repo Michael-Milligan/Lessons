0038928d8c4dcb2f31afe661f748a3cd2204c498
Michael-Milligan <62278978+Michael-Milligan@users.noreply.github.com>
2021-01-03 00:16:54 +0300

    Began Testing

---
+++ Web/WebApplications/VSC/CLI/Tests/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Testing.Models;
+
+namespace Testing.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ViewResult Index()
+        {
+            return View(
+                new List<Product>()
+                {
+                    new Product(){Name = "Ha", Price = 15},
+                    new Product(){Name = "Ho", Price = 25}
+                }
+            );
+        }
+    }
+}
